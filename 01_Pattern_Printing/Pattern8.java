2e664bbabccb363e029ea4d26e2fbeeb15ff579e
Vishal-Chavan7 <96495893+Vishal-Chavan7@users.noreply.github.com>
2025-06-09 12:44:09 +0530

    Pattern8 question done

--- 01_Pattern_Printing/Pattern8.java
+++ 01_Pattern_Printing/Pattern8.java
@@ -3,13 +3,13 @@ public class Pattern8 {
         int n=5;
         for(int i=1; i<=n; i++){
 
-            for(int j=1; j<=i; j++){
-                System.out.print(" ");
+            for(int j=1; j<i; j++){
+                System.out.print(".");
             }
 
             for(int k=1; k<=(2*n)-(2*i-1); k++) {
-                System.out.print("*");
-            }
+    System.out.print("*");
+}
             System.out.println();
         }
     }
