171583babafa86bd12ca69e53b3c775b954d4728
Vishal-Chavan7 <96495893+Vishal-Chavan7@users.noreply.github.com>
2025-06-25 21:15:11 +0530

    Optimized Approach

--- 06_Array_Advanced_Question/Medium_FAQ/twoSum.java
+++ 06_Array_Advanced_Question/Medium_FAQ/twoSum.java
@@ -1,17 +1,36 @@
+import java.util.*;
 public class twoSum {
 
-    public static int[] TwoSum(int[] nums, int target){
+     public static int[] TwoSum(int [] nums, int target){
         int n = nums.length;
-        
-        for(int i =0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i,j};
-                }
+        Map <Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i<n; i++){
+            int num = nums[i];
+            int complement = target - num;
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
             }
+            map.put(num, i);
         }
-        return new int[]{-1,-1};
-    }
+
+        return new int[]{-1, -1}; // If no solution is found
+
+
+     }
+
+    // public static int[] TwoSum(int[] nums, int target){
+    //     int n = nums.length;
+        
+    //     for(int i =0; i<n; i++){
+    //         for(int j=i+1; j<n; j++){
+    //             if(nums[i] + nums[j] == target){
+    //                 return new int[]{i,j};
+    //             }
+    //         }
+    //     }
+    //     return new int[]{-1,-1};
+    // }
 
     public static void main(String[] args) {
         int [] nums = {2,16,11,7,5,3,9,4};
